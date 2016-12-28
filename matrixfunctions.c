d2f9b0d08f788711a50f329953733a2da0b44790
stchristian <hubner.krisztian97@gmail.com>
2016-12-28 14:22:08 +0100

    Update matrixfunctions.c

--- matrixfunctions.c
+++ matrixfunctions.c
@@ -1,54 +1,76 @@
 #include "datatypes.h"
 #include <math.h>
 
-Matrix* GaussElimination(Matrix* mtxin){
-    Matrix* mtxresult = AllocateNewMtx(mtxin->rows, mtxin->columns);
-    CopyMtx(mtxresult,mtxin);
+Matrix* MTX_GaussElim(Matrix* mtxin){
+    Matrix* mtxresult = MTX_Alloc(mtxin->rows, mtxin->columns);
+    MTX_Copy(mtxresult,mtxin);
 
     //3 fazisu Gauss-eliminacio
-    enum States{FIRST,SECOND,THIRD};
+    enum States{LOOP,CHANGEROW,FINISHED};
 
-    enum States state = FIRST;
+    enum States state = LOOP;
     bool finished = false;
     int i = 0,j = 0;
     int row,col;
     while(finished){
         switch(state){
             //Elso fazis
-            case FIRST:
+            case LOOP:
                 //ha szam == 0, ugrunk a masodik fazisra
                 if(fabs(0.f - mtxresult->numbers[i][j]) < 1.e-9){
-                    state = SECOND;
+                    state = CHANGEROW;
                     break;
                 }
-                //egyebkent beszorozzuk a sort a reciprokaval, es harmadik fazisba lepunk ha ez volt az utolso sor
+                //egyebkent beszorozzuk a sort az aktualis elem reciprokaval, es harmadik fazisba lepunk ha ez volt az utolso sor
                 else{
-                    for(col = 0; col < mtx->columns; col++){
+                    for(col = j; col < mtx->columns; col++){
                         mtxresult->numbers[i][col] = mtxresult->numbers[i][col] * (1.f / mtxresult->numbers[i][j]);
                     }
                     if(i == mtxresult->rows - 1){
-                        state = THIRD;
+                        state = FINISHED;
                         break;
                     }
                 }
                 //kinullazzuk az alatta levo sorokat
                 for(row = i + 1; row < mtxresult->rows; row++){
-                    for(col = j; col < mtxresult->columns; col++){
-                        if(!(fabs(0.f - mtxresult->numbers[row][j]) < 1.e-9)){
+                    if(!(fabs(0.f - mtxresult->numbers[row][j]) < 1.e-9)){
+                        for(col = j; col < mtxresult->columns; col++){
                             mtxresult->numbers[row][col] = mtxresult->numbers[row][col] - mtxresult->numbers[row][j]*mtxresult->numbers[i][col];
                         }
                     }
                 }
                 //ha mar nincs tobb oszlop, 3. fazisra ugras
                 if(j == mtxresult->columns - 1){
-                    state = THIRD;
+                    state = FINISHED;
                     break;
                 }
                 i++,j++;
                 break;
-            case SECOND:
+            case CHANGEROW:
+                if(i < mtxresult->rows-1){
+                    row = i + 1;
+                    bool notzero = false;
+                    while(!notzero && row < mtxresult->rows){
+                        if(!(fabs(0.f - mtxresult->numbers[row][j]) < 1.e-9)){
+                            notzero = true;
+                        }
+                        else row++;
+                    }
+                    if(notzero){
+                        MTX_ChangeRow(&mtxresult,i,row);
+                        state = LOOP;
+                        break;
+                    }
+                }
+                else if(j == mtxresult->columns - 1){
+                    i--;
+                    state = FINISHED;
+                    break;
+                }
+                j++;
+                state = LOOP;
                 break;
-            case THIRD:
+            case FINISHED:
                 break;
         }
     }
