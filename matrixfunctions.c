3d83c5719010d863954dae1aa6a786cc93eceec1
stchristian <hubner.krisztian97@gmail.com>
2016-12-28 17:41:59 +0100

    Add files via upload

---
+++ matrixfunctions.c
@@ -0,0 +1,15 @@
+#include "datatypes.h"
+#include <stdlib.h>
+
+Matrix* MTX_Malloc(int r, int c){
+    Matrix* newmtx = malloc(sizeof(Matrix));
+    newmtx->columns = c;
+    newmtx->rows = r;
+    newmtx->numbers = (double**)malloc(r * sizeof(double*));
+    newmtx[0] = (double*) malloc(r * c * sizeof(double));
+    int rowIter;
+    for (rowIter = 1; rowVar < r; rowIter++){
+        newmtx[rowIter] = newmtx[0] + rowIter*c;
+    }
+    return newmtx;
+}
