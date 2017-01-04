3978bf8addbe5c72ee11267739c5620e64a5b84b
stchristian <hubner.krisztian97@gmail.com>
2017-01-04 23:48:58 +0100

    Update prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -11,4 +11,19 @@ int FloatCmp(double a, double b,double precision);
 
 void MTX_ConsolePrintMtx(Matrix* mtx);
 Matrix* MTX_ConsoleInp();
+
+/*Ha a megadott mátrix invertálható, akkor igaz értéket ad vissza.*/
+bool IsInvertable(Matrix* mtxA);
+
+/*Ha a megadott mátrix négyzetes, akkor igaz értéket ad vissza.*/
+bool IsQuadratic(Matrix* mtxQ);
+
+/*Ha a megadott mátrix diagonális, akkor igaz értéket ad vissza.*/
+bool IsDiagonal(Matrix* mtxD);
+
+/*Ha a megadott mátrixok típusa megegyezik, akkor összeadhatóak és igaz értéket ad vissza.*/
+bool AddCheck(Matrix* mtxA, Matrix* mtxB);
+
+/*Ha a megadott mátrixok összeszorozhatóak, akkor igaz értéket ad vissza.*/
+bool MultiCheck(Matrix* mtxA, Matrix* mtxB);
 #endif // PROTOTYPES_H_INCLUDED
