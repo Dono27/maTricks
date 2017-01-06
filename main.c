7d9f9b333e4cf27fd6de310a3ad784194f581b4f
stchristian <hubner.krisztian97@gmail.com>
2017-01-06 13:33:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include "MTX.h"
+#include <stdio.h>
 
 int main(void){
     Matrix* mtx = MTX_ConsoleInp();
 
     MTX_ConsolePrintMtx(mtx);
 
-    mtx = MTX_GaussElim(mtx, SOLVE_LINEAR_SYSTEM);
+    mtx = MTX_GaussElim(mtx, CALCULATE_DET);
 
     MTX_ConsolePrintMtx(mtx);
-
-
-
-
+    printf("\nDeterminans : %f", mtx->determinant);
 
     return 0;
 }
