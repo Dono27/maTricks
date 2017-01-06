811f2cd2447c4456073f7db0ff368a15ba812558
stchristian <hubner.krisztian97@gmail.com>
2017-01-06 09:34:26 +0100

    Update matrix_basicfunc_ptypes.h

--- matrix_basicfunc_ptypes.h
+++ matrix_basicfunc_ptypes.h
@@ -7,20 +7,22 @@ Matrix* MTX_Malloc(int r, int c);
 void MTX_DeleteRow(Matrix* mtx, int rowNo);
 void MTX_ChangeRow(Matrix* mtx, int rowA, int rowB);
 void MTX_Copy(Matrix* mtxTo, Matrix* mtxFrom);
+void MTX_AddRow(Matrix* mtx, int i, int t, double lambda);
+void MTX_MultiplyRow(Matrix* mtx, int i, double lambda);
 
-/*Ha a megadott m·trix invert·lhatÛ, akkor igaz ÈrtÈket ad vissza.*/
+/*Ha a megadott m√°trix invert√°lhat√≥, akkor igaz √©rt√©ket ad vissza.*/
 bool IsInvertable(Matrix* mtxA);
 
-/*Ha a megadott m·trix nÈgyzetes, akkor igaz ÈrtÈket ad vissza.*/
+/*Ha a megadott m√°trix n√©gyzetes, akkor igaz √©rt√©ket ad vissza.*/
 bool IsQuadratic(Matrix* mtxQ);
 
-/*Ha a megadott m·trix diagon·lis, akkor igaz ÈrtÈket ad vissza.*/
+/*Ha a megadott m√°trix diagon√°lis, akkor igaz √©rt√©ket ad vissza.*/
 bool IsDiagonal(Matrix* mtxD);
 
-/*Ha a megadott m·trixok tÌpusa megegyezik, akkor ˆsszeadhatÛak Ès igaz ÈrtÈket ad vissza.*/
+/*Ha a megadott m√°trixok t√≠pusa megegyezik, akkor √∂sszeadhat√≥ak √©s igaz √©rt√©ket ad vissza.*/
 bool SizeEqCheck(Matrix* mtxA, Matrix* mtxB);
 
-/*Ha a megadott m·trixok ˆsszeszorozhatÛak, akkor igaz ÈrtÈket ad vissza.*/
+/*Ha a megadott m√°trixok √∂sszeszorozhat√≥ak, akkor igaz √©rt√©ket ad vissza.*/
 bool MultiCheck(Matrix* mtxA, Matrix* mtxB);
 
 #endif // MATRIX_BASICFUNC_PTYPES_H_INCLUDED
