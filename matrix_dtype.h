b2c81bcd412cae920fc9241017dc782d82aae974
stchristian <hubner.krisztian97@gmail.com>
2017-01-06 13:29:46 +0100

    Update matrix_dtype.h

--- matrix_dtype.h
+++ matrix_dtype.h
@@ -1,8 +1,9 @@
 #ifndef MATRIX_DTYPE_H_INCLUDED
 #define  MATRIX_DTYPE_H_INCLUDED
 
+//FLAG-ek a MTX_GaussElim()-hez
 #define SOLVE_LINEAR_SYSTEM 0x01
-
+#define CALCULATE_DET 0x02
 
 /**A matrix.
 *Ez az adatszerkezet egy matrix adatait tarolja el.
