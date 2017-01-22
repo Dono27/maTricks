b89dff85294a40cc203fa6e292b9f5aa229bcde4
stchristian <hubner.krisztian97@gmail.com>
2017-01-22 15:16:15 +0100

    MTX_ Előtagok kiegészítése pipa.

--- matrix_basicfunc_ptypes.h
+++ matrix_basicfunc_ptypes.h
@@ -25,13 +25,13 @@ void MTX_AddRow(Matrix* mtx, int i, int t, double lambda);
 void MTX_MultiplyRow(Matrix* mtx, int i, double lambda);
 
 /*Ha a megadott mátrixok típusa megegyezik, akkor összeadhatóak és igaz értéket ad vissza.*/
-bool SizeEqCheck(Matrix* mtxA, Matrix* mtxB);
+bool MTX_SizeEqCheck(Matrix* mtxA, Matrix* mtxB);
 
 /*Ha a két megadott mátrix összeadható, akkor az összegmátrixot adja eredményül.*/
 Matrix* MTX_Sum(Matrix* mtxA, Matrix* mtxB);
 
 /*Ha a megadott két mátrix összeszorozhatóak, akkor igaz értéket ad vissza.*/
-bool MultiCheck(Matrix* mtxA, Matrix* mtxB);
+bool MTX_MultiCheck(Matrix* mtxA, Matrix* mtxB);
 
 /*Ha a megadott két mátrix összeszorozható, akkor a szorzat eredménymátrixát adja eredményül. Címet ad vissza.*/
 Matrix* MTX_Multiply(Matrix* mtxA, Matrix* mtxB);
@@ -55,46 +55,46 @@ Matrix* MTX_HadamardMultiply(Matrix* mtxA, Matrix* mtxB);
 Matrix* MTX_Transposition(Matrix* mtxA);
 
 /*Ha a megadott két mátrix minden eleme egyenlő, akkor igaz értéket ad vissza.*/
-bool IsEqual(Matrix* mtxA, Matrix* mtxB);
+bool MTX_IsEqual(Matrix* mtxA, Matrix* mtxB);
 
 /*Ha a megadott mátrix invertálható, akkor igaz értéket ad vissza.*/
-bool IsInvertable(Matrix* mtxA);
+bool MTX_IsInvertable(Matrix* mtxA);
 
 /*Ha a megadott mátrix négyzetes, akkor igaz értéket ad vissza.*/
-bool IsQuadratic(Matrix* mtxQ);
+bool MTX_IsQuadratic(Matrix* mtxQ);
 
 /*Ha a megadott mátrix diagonális, akkor igaz értéket ad vissza.*/
-bool IsDiagonal(Matrix* mtxD);
+bool MTX_IsDiagonal(Matrix* mtxD);
 
 /*Ha a megadott mátrix egységmátrix, akkor igaz értéket ad vissza.*/
-bool IsIdentity(Matrix* mtxI);
+bool MTX_IsIdentity(Matrix* mtxI);
 
 /*type: 0 vagy 1
   Ha a megadott mátrix főátlója csak a megadott (0 vagy 1) elemeket tartalmaz, akkor igaz értéket ad vissza.*/
-bool DiagonalType(Matrix* mtxI, double type);
+bool MTX_DiagonalType(Matrix* mtxI, double type);
 
 /*Ha a megadott mátrix szimmetrikus, akkor igaz értéket ad vissza.*/
-bool IsSymmetric(Matrix* mtxS);
+bool MTX_IsSymmetric(Matrix* mtxS);
 
 /*Ha a megadott mátrix antiszimmetrikus (ferdén szimmetrikus), akkor igaz értéket ad vissza.*/
-bool IsAntiSymmetric(Matrix* mtxA);
+bool MTX_IsAntiSymmetric(Matrix* mtxA);
 
 /*Igaz értéket ad vissza, ha a megadott mátrix főátlója alatti elemek mind nullák.*/
-bool IsUpperTriangle(Matrix* mtxT);
+bool MTX_IsUpperTriangle(Matrix* mtxT);
 
 /*Igaz értéket ad vissza, ha a megadott mátrix főátlója feletti elemek mind nullák.*/
-bool IsLowerTriangle(Matrix* mtxT);
+bool MTX_IsLowerTriangle(Matrix* mtxT);
 
 /*Ha a megadott mátrix háromszögmátrix (alsó vagy felső), akkor igaz értéket ad vissza.*/
-bool IsTriangular(Matrix* mtxT);
+bool MTX_IsTriangular(Matrix* mtxT);
 
 /*Ha a megadott mátrix háromszögmátrix, főátlója csak nullákat tartalmaz, a nem a háromszöget alkotó elemek pedig mind nullák, akkor igaz értket ad vissza.*/
-bool IsStrictlyTriangular(Matrix* mtxT);
+bool MTX_IsStrictlyTriangular(Matrix* mtxT);
 
 /*Ha a megadott mátrix háromszögmátrix, főátlója csak egyeseket tartalmaz, a nem háromszöget alkotó elemek pedig mind nullák, akkor igaz értéket ad vissza.*/
-bool IsUnitriangular(Matrix* mtxT);
+bool MTX_IsUnitriangular(Matrix* mtxT);
 
 /*Ha a kapott mátrix nullmátrix (minden eleme nulla), akkor igaz értéket ad vissza.*/
-bool IsZero(Matrix* mtxZ);
+bool MTX_IsZero(Matrix* mtxZ);
 
 #endif // MATRIX_BASICFUNC_PTYPES_H_INCLUDED
